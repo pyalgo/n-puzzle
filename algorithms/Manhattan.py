6d4567aac8d3e1ea6ebef17516e8e3ac0ea96294
Oleksandr Baranov <oleksandr.baranov@smashdocs.net>
2018-07-19 01:08:41 +0300

    some fix

--- algorithms/Manhattan.py
+++ algorithms/Manhattan.py
@@ -26,6 +26,6 @@ def manhattan_district(parent_node, full_path):
     full_path.append(parent_node)
     best_child = get_best_child(parent_node)
     if not best_child.grid is generate_solved_puzzle(len(best_child.grid), len(best_child.grid)):
-        return manhattan_district(parent_node, full_path)
+        return manhattan_district(best_child, full_path)
     else:
         return full_path
